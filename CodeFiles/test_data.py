87a5d507e7e5aee7af1c2a5b90554afa8be5d978
Abhijeet <afarane@gmail.com>
2018-04-10 16:44:24 +0530

    Update test_data.py

--- CodeFiles/test_data.py
+++ CodeFiles/test_data.py
@@ -1,12 +1,15 @@
 # =========================== Part 2: Prediction ===========================
-var_test_image = 'data/predict/d2.png'
+var_test_image = 'data/predict/4.jpg'
 
 import numpy as np
+import os, cv2, random
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
-from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
 
+# --------- Define Input Parameters ---------
+img_width, img_height = 150, 150
 
 def get_image(path):
     img = image.load_img(path, target_size=(img_width, img_height))
@@ -15,21 +18,20 @@ def get_image(path):
     x = preprocess_input(x)
     return img, x
  
+
 img, x = get_image(var_test_image)    
-#model = load_model("data/output/Model.h5")
-result= model.predict(x)
-imshow(img)
- 
-training_set.class_indices
+model = load_model("data/output/Model.h5")
+predictions= model.predict(x)
+plt.imshow(img)
+#plt.show()	
 
 # Result
-result[0][0]
-if result[0][0] ==1:
-    prediction = 'File01'
-    print("prediction = " + prediction)
+
+predictions[0][0]
+if predictions[0][0] == 0:      
+	print('I think this is a Cat')
 else:
-    prediction = 'File02'
-    print("prediction = " + prediction)
-    
+    print('I think this is a Dog')
+
 
 # ----------------------------------------------------------------
