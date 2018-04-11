55f6a7698cc5b2a00f6d118f86a91c0fb4562635
Abhijeet <afarane@gmail.com>
2018-04-11 18:49:18 +0530

    Update train_data_2.py

--- CodeFiles/train_data_2.py
+++ CodeFiles/train_data_2.py
@@ -38,12 +38,12 @@ model.add(Conv2D(16, (3, 3), padding="same",activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
-# Layer 3
+# Layer 2
 model.add(Conv2D(32, (3, 3), padding="same",activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
-# Layer 4
+# Layer 3
 model.add(Conv2D(64, (3, 3), padding="same",activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
