574a4ecf692cf24c14f926334d74b2e9122e7406
Abhijeet <afarane@gmail.com>
2018-04-11 14:42:16 +0530

    Update train_data.py

--- CodeFiles/train_data.py
+++ CodeFiles/train_data.py
@@ -1,20 +1,21 @@
 from keras.models import Sequential
+#from keras import optimizers
 from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
 from keras import backend as K
 import matplotlib.pyplot as plt
 
 # --------- Define Input Parameters ---------
 img_width, img_height = 150, 150
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
+# ----
+train_data_dir = 'train4k'
+validation_data_dir = 'validation0.8k'
+# ----
 nb_train_samples = 4000
-nb_validation_samples = 900
-epoch = 30
-batch_size = 400
+nb_validation_samples = 800
+batch_size = 100
+epoch = 50
 
-# Model.Compile Parameters :
-optimizer = 'adam'
-loss='binary_crossentropy'
+# ----
 
 # --------- Define Backend ---------
 # if channels_first then Theano , if channels_last then TensorFlow
@@ -51,11 +52,12 @@ model.add(Dense(units=64,activation='relu'))
 model.add(Dense(units=64,activation='relu'))
 model.add(Dropout(0.2))
 
+
 # Final / Output Layer
 model.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling the CNN
-model.compile(optimizer=optimizer,loss=loss, metrics=['accuracy'])
+model.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
 
 
 # --------- data augmentation ---------
@@ -86,18 +88,20 @@ validation_set = validation_datagen.flow_from_directory(
     class_mode='binary')
 
 # 3. Create a loss history
+
 history  = model.fit_generator(
     training_set,
-    steps_per_epoch=nb_train_samples // batch_size, # batch_size
+    steps_per_epoch=nb_train_samples // batch_size, # // batch_size
     epochs=epoch,
 	verbose=2,
 	validation_data=validation_set,
-    validation_steps=nb_validation_samples // batch_size)
+    validation_steps=nb_validation_samples // batch_size) # // batch_size
 	
+
 
 # ======================== Save Model ==============================
 #model.save_weights("output/Model_weights.h5")
-model.save("data/output/Model.h5",True)
+model.save("output/Model.h5",True)
 print("Saved model to disk")
 
 print("validation_set class_indices = {} ".format(validation_set.class_indices))
@@ -110,9 +114,9 @@ plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
-plt.savefig("data/output/PLTResult.jpg")
+plt.savefig("output/PLTResult.jpg")
 print("Saved model to disk")
-plt.show()
+#plt.show()
 
 # ----------------------------------------------------------------
 print('Training Finished ! ')
